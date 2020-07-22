6c9056e8dd665faed61296747b3214d267c630f2
Camila <caming2309@gmail.com>
2020-07-22 01:14:50 -0300

    add footer

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const icon = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.elements-container');
 
+
 icon.addEventListener('click', () => {
     console.log('holis')
     navLinks.classList.toggle('visible')
 });
 
-
+window.sr = ScrollReveal();
+  sr.reveal('.middle-content', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
